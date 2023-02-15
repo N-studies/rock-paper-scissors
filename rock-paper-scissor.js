472344b0abff99c56a3513af03c6a03894351635
N-studies <122186098+N-studies@users.noreply.github.com>
2023-02-15 11:43:22 -0500

    Make point system and announce winner

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -17,30 +17,53 @@ buttons.forEach((button) => {
   button.addEventListener("click", () => {
     let playerSelection = button.id;
     let computerChoice = getComputerChoice();
-    const buttonContainer = document.querySelector("#button-container");
-    console.log(playRound(playerSelection, computerChoice));
     const round = document.createElement("div");
+    const buttonContainer = document.querySelector("#button-container");
     round.classList.add("round");
     round.textContent = playRound(playerSelection, computerChoice);
     buttonContainer.appendChild(round); //may be able to delete child
-    
+    let winner;
+    if (computerScore == 5) {
+      winner = document.createElement("div");
+      winner.textContent = "computer won"
+      buttonContainer.appendChild(winner);
+      
+    }
+    else if (playerScore == 5){
+      winner = document.createElement("div");
+      winner.textContent = "you won"
+      buttonContainer.appendChild(winner);
+      
+    }
   });
 });
+//ADD POINT SYSTEM TO playRound instead of button?
 /*------------------Gameplay------------------*/
-
+let playerScore = 0;
+let computerScore = 0;
 function playRound(playerSelection, computerChoice) {
   if (
     (playerSelection == "rock" && computerChoice == "scissors") ||
     (playerSelection == "paper" && computerChoice == "rock") ||
     (playerSelection == "scissors" && computerChoice == "paper")
   ) {
-    return `you win! You chose ${playerSelection}, the computer chose ${computerChoice}`;
+    return `you win! You chose ${playerSelection}, the computer chose ${computerChoice}. your score ${++playerScore}`;
   } else if (playerSelection == computerChoice) {
-    return `tie!  You chose ${playerSelection}, the computer chose ${computerChoice}`;
+    return `tie! You chose ${playerSelection}, the computer chose ${computerChoice}. `;
   } else {
-    return `you lose!  You chose ${playerSelection}, the computer chose ${computerChoice}`;
+    return `you lose! You chose ${playerSelection}, the computer chose ${computerChoice}. computer score ${++computerScore}`;
   }
 }
+
+
+
+ 
+    
+   
+  
+  
+  
+
 /*
 function game() {
   let playerScore = 0;
